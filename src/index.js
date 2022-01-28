a4cb2b96217eb68197d530d86cda646f3d6b28b2
egor <spirin.egor13@gmail.com>
2022-01-28 20:31:24 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let bracketsPair = {}
+  let openBrackets = [] 
+
+  for (let i of bracketsConfig) {
+      openBrackets.push(i[0])
+      bracketsPair[i[1]] = i[0]
+  }
+
+  for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      
+      if (bracketsPair[currentSymbol] === currentSymbol && !stack.includes(currentSymbol)) {
+        stack.push(currentSymbol)
+      } else if (bracketsPair[currentSymbol] === currentSymbol && stack.includes(currentSymbol)) {
+        if (stack.length === 0) {
+          return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (bracketsPair[currentSymbol] === topElement) {
+          stack.pop();
+      } else {
+          return false;
+      }
+      } else if (openBrackets.includes(currentSymbol)) {
+           stack.push(currentSymbol);
+      } else {
+
+          if (stack.length === 0) {
+              return false;
+          }
+
+          let topElement = stack[stack.length - 1];
+
+          if (bracketsPair[currentSymbol] === topElement) {
+              stack.pop();
+          } else {
+              return false;
+          }
+      }
+      console.log(stack)
+
+  }
+
+  return stack.length === 0;
 }
